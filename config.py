675c2f6b0bf3ec42bff74b369d9ddbbef8e81033
Grealish17 <ark-gto@yandex.ru>
2022-04-27 15:22:06 +0300

    WORK

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 db_host=os.environ["HOST"]
-db_user=os.environ["MYSQL_USER"]
-db_password=os.environ["MYSQL_PASSWORD"]
-db_name=os.environ["MYSQL_NAME"]
+db_user=os.environ["USER"]
+db_password=os.environ["PASSWORD"]
+db_name=os.environ["NAME"]
 db_admin_id=os.environ["ADMIN_ID"]
 db_token=os.environ["TOKEN"]
 PROXY_URL = ''
